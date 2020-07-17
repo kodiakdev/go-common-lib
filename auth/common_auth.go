96968306c284cc3df1ce8c3a85654c5da28cd670
kodiakdev <dev.kodiak@gmail.com>
2020-07-17 10:09:53 +0700

    add common auth to extract requester id

---
+++ auth/common_auth.go
@@ -0,0 +1,23 @@
+package commonauth
+
+import (
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"github.com/emicklei/go-restful"
+)
+
+const (
+	RequesterUserID = "requesterUserId"
+)
+
+func ExtractRequesterID(req *restful.Request) primitive.ObjectID {
+	iUserID := req.Attribute(RequesterUserID)
+	userID := fmt.Sprintf("%v", iUserID)
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		id, _ = primitive.ObjectIDFromHex("")
+	}
+	return id
+}
